d5043ec91c1d86f03c6cac6da9f6406fb1898cb3
Colink02 <colinkinzel@gmail.com>
2018-08-01 20:55:12 -0500

    First App

--- ColinKWebsite/Home/views.py
+++ ColinKWebsite/Home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hola People!")
+    now = datetime.now()
+    return render(
+        request,
+        "index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Everyone",
+            'message' : "This is my data! ",
+            'content': "on " + now.strftime("%A, %d %B, %Y at %X")
+        })
+
 # Create your views here.
